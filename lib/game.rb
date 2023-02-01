ee73b4e8a53857b18d4c919a0f46a3f2531cff0c
Morfenza <ianlourenco@protonmail.com>
2023-02-01 17:32:41 -0300

    Tweak initialize method

--- lib/game.rb
+++ lib/game.rb
@@ -8,25 +8,28 @@ require_relative 'messages_module'
 class Game
   include Messages
 
-  attr_reader :player1, :player2, :board
+  attr_reader :player1, :player2
 
   def initialize
     @player1 = Player.new(ask_name(1), ask_symbol)
     @player2 = Player.new(ask_name(2), ask_symbol)
     @board = Board.new
+    puts @board.display_board
   end
 
-  def play_round(board, player)
-    board.insert_symbol(player.symbol, ask_space(player) - 1)
-    puts board.display_board
-    return finish_game(player) if check_victory(board).eql?(player.symbol)
+  # Single round is played
+  def play_round(player)
+    @board.insert_symbol(player.symbol, ask_space(player) - 1)
+    puts @board.display_board
+    return finish_game(player) if check_victory.eql?(player.symbol)
   end
 
-  def check_victory(board)
+  # Generic victory check
+  def check_victory
     Board::WINNING_LINES.each do |line|
-      if board.spaces[line[0]] == board.spaces[line[1]] &&
-         board.spaces[line[0]] == board.spaces[line[2]]
-        return board.spaces[line[0]]
+      if @board.spaces[line[0]] == @board.spaces[line[1]] &&
+         @board.spaces[line[0]] == @board.spaces[line[2]]
+        return @board.spaces[line[0]]
       end
     end
   end
